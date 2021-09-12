87b528d8a94398ebb6d782250d4900423fc8544f
Wannes Dieltiens <50752071+wannesds@users.noreply.github.com>
2021-09-12 13:12:08 +0200

    synch

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,18 @@
-// contract test code will go here
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3'); //capital W because constructor func
+const web3 = new Web3(ganache.provider()); //small w bceause instance
+
+class Car {
+    park() {
+        return 'stopped';
+    }
+
+    drive() {
+        return 'vroom';
+    }
+}
+
+describe('Car', () => {
+    it('can park', ())
+});
